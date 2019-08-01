3a4b001475e5f3fdeb69483fd5f8a2cf27f3e980
Minho-Yeon <wptnzpq@gmail.com>
2019-08-01 12:53:29 +0900

    no message

--- src/contents/Login.js
+++ src/contents/Login.js
@@ -1,9 +1,17 @@
 import React,{Component} from 'react';
+import AuthButton from './auth/AuthButton.js';
+import AuthContent from './auth/AuthContent.js';
+import InputWithLabel from './auth/InputWithLabel.js';
+import './Login.scss';
 class Login extends Component{
     render(){
         return(
-            <div>
-                Login
+            <div className='loginform'>
+            <AuthContent title="로그인">
+                <InputWithLabel label="이메일" name="email" placeholder="이메일"/>
+                <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
+                <AuthButton>로그인</AuthButton>
+            </AuthContent>
             </div>
         )
     }
