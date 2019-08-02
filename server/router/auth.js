63cca945cea98e52409594c11b2acef7fcae7adc
dr <love.pink.edacb1@gmail.com>
2019-08-02 16:08:22 +0900

    -

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login/:pw/:salt', (req, res)=> {
+router.get('/:pw/:salt', (req, res)=> {
     console.log(req.params.salt)
     console.log(req.params.pw)
     res.redirect('http://localhost:3000/login')
