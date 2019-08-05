e9b141e10babc53f2b5502ebd6e47e4fbd40919b
Minho-Yeon <wptnzpq@gmail.com>
2019-08-05 17:27:49 +0900

    no message

--- src/Auth.js
+++ src/Auth.js
@@ -12,7 +12,7 @@ class Auth extends Component {
                 <h1> 환영합니다! </h1>
                 <p> 계정이 확인되어 이제 활성화되었습니다. </p>
                 <p> 시작하려면 로그인하십시오. </p>
-                <Button className='btn' color="primary" size="lg" block>Block level button</Button>
+                <Button className='btn' color="primary" size="lg" onClick={()=>{this.props.hitory.props('/login')}} block>Block level button</Button>
             </div>
         );
     }
