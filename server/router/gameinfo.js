a046460a1bbc2e250f757f0e1fa3f550b66ecfe6
Minho-Yeon <wptnzpq@gmail.com>
2019-08-05 17:22:48 +0900

    no message

--- server/router/gameinfo.js
+++ server/router/gameinfo.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/',(req,res)=>{
     console.log('/server/gameinfo');
+    console.log(gamelist);
     res.json({'gamelist':gamelist});
 });
 
--- src/contents/Request.js
+++ src/contents/Request.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
-const request = async (type,url,data)=>{  //axios를 간편하게 사용할 수 있도록 만들어 놓은 모듈- 민호
-    await axios[type](url,data)
-    .then(c=>{
-        return c;
-    })
-    .catch(err=>{
-        console.log(err);
-        return err;
+const request =(type, url, data) => {  //axios를 간편하게 사용할 수 있도록 만들어 놓은 모듈- 민호
+    return new Promise((resolve, reject) => {
+       axios[type](url, data)
+            .then(c => {
+                // console.log(c);
+                resolve(c);
+                // return c;
+                
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            })
+            
     })
 }
 
