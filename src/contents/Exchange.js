8606c924cb7418a46aa76dad78eff5bb53e4d90e
hwangsanguk <lall21@naver.com>
2019-08-05 20:18:39 +0900

    no message

--- src/contents/Exchange.js
+++ src/contents/Exchange.js
@@ -1,16 +1,45 @@
 // 환전소 폼 시작 상욱(08.02)
-import React,{Component} from 'react'; 
+import React, { Component } from 'react';
 import Exchangelist from './Exchangelist.js';
 import './Exchange.scss';
+import {Link} from 'react-router-dom';
 
 
-class Exchange extends Component{
-    render(){
-        return(
+class Exchange extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            trade :""
+
+        };
+    };
+
+    click1 = () => {
+        var langSelect = document.getElementById('trade');
+        console.log('log :', langSelect);
+        var selectValue = langSelect.options[langSelect.selectedIndex].value;
+        console.log(selectValue);
+        var selectText = langSelect.options[langSelect.selectedIndex].text;
+        console.log(selectText);
+        this.setState({
+                trade : selectValue
+        });
+    }
+
+    render() {
+        return (
             <div className="Exchange">
-                    <Exchangelist number="0"/>
-          
-                   </div>
+                <h1>거래소</h1>
+                <select id="trade" onChange={this.click1}>
+                    <option value="" > 선택</option>
+                    <option value="localhost:3000/exchangeGameMony" > 게임머니로 바꾸기</option>
+                    <option value="pointtrade">포인트로 바꾸기</option>
+                   
+                </select>
+                    {this.state.trade}
+                <Exchangelist number="0" />
+
+            </div>
         )
     }
 }
--- src/contents/Exchangelist.js
+++ src/contents/Exchangelist.js
@@ -4,29 +4,32 @@ import './Exchange.js';
 
 class Exchangelist extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            output:0,
+        this.state = {
+            output: 0,
         }
     }
- datain=()=>{
-       
-        var val = document.getElementsByClassName('datain')[0] ;
+
+
+    datain = () => {
+
+        var val = document.getElementsByClassName('datain')[0];
         console.log("value=", val.value);
-        var out =  document.getElementsByClassName('out');
-       
-        out.value = Number(val.value)/10;
-           console.log("value=", out);
-   
-           this.setState({
-               output:out.value,
-           });
-   
-       }
-    render() {
-     var props = this.props.number
-     function click(){
+        var out = document.getElementsByClassName('out');
+        var change = document.getElementsByClassName('aaabbb')[0];
+        console.log(change);
+
+        out.value = Number(val.value) * change.value;
+        console.log("value=", out);
+
+        this.setState({
+            output: out.value,
+        });
+    }
+
+    click = () => {
+        var props = this.props.number
         if (document.getElementsByClassName('checkbox')[props].checked) {
             document.getElementsByClassName('closebox')[props].style.display = "block";
             document.getElementsByClassName('closebox')[props].style.height = "100%";
@@ -36,24 +39,42 @@ class Exchangelist extends Component {
             document.getElementsByClassName('closebox')[props].style.display = "none";
             document.getElementsByClassName('openbox')[props].style.width = "100%";
         }
+
+        // addBtn = () =>{
+        //     console.log("closebox =",document.getElementsByClassName('closebox'));
+
+        // }
+
     }
-    
+    render() {
+
 
         return (
             <div>
+
                 <div className='relativeBox'>
-                    
+
                     <div className='openbox' >
                         <img src="./jpg/maplestory.jpg" alt="" />
-                        {this.props.number}
-                        <input type="checkbox" className="checkbox" onClick={click}/>
+                        {/* 게임 이름과 환율이 들어갈 자리 */}
+                        <h2>메이플 스토리</h2>
+                        <h2>환율: 1P = </h2>
+                        {/*  */}
+
+                        <input className="aaabbb"></input>
+                        <input type="checkbox" className="checkbox" onClick={this.click} />
+                        <hr />
                     </div>
                     <div className='closebox'>
-                        <input  className="datain" onKeyUp={this.datain} />
-                        <p className="out">{this.state.output}</p>
-                        
-                        <input type="checkbox" />
-                        <input type="number" />
+                        <select >
+                            <option value="charic1"> 캐릭터1</option>
+                            <option value="charic2"> 캐릭터2</option>
+                            <option value="charac3"> 캐릭터3</option>
+                        </select>
+                        <input className="datain" onKeyUp={this.datain} />
+                        <img src="./jpg/rightarrow.png" className="right" alt="" />
+                        <input className="output" type="text" value={this.state.output} />
+                        <button className="addBtn" onClick={this.addBtn}>추가하기</button>
                     </div>
                 </div>
 
