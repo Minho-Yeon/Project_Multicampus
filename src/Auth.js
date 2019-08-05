a88c9a0a81a7380c05337b95821e242e804ca8f6
dr <love.pink.edacb1@gmail.com>
2019-08-05 17:23:43 +0900

    Update Auth.js

--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Auth.scss';
+import { Button } from 'reactstrap';
 
 class Auth extends Component {
 
@@ -7,11 +8,14 @@ class Auth extends Component {
 
     render() {
         return (
-            <div className="">
-                
+            <div className="Auth">
+                <h1> 환영합니다! </h1>
+                <p> 계정이 확인되어 이제 활성화되었습니다. </p>
+                <p> 시작하려면 로그인하십시오. </p>
+                <Button className='btn' color="primary" size="lg" block>Block level button</Button>
             </div>
         );
     }
 }
 
-export default Footer;
+export default Auth;
