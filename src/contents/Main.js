0094edc7cdbc02cf1e6c910ef7bc6ca78520d7ec
Minho-Yeon <wptnzpq@gmail.com>
2019-08-16 14:12:40 +0900

    no message

--- src/contents/Main.js
+++ src/contents/Main.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom'
 import './Main.scss';
 import LoginModal from './LoginModal';
+import request from './Request.js';
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -12,27 +13,101 @@ class Main extends Component {
             modal: false,
             gamelist: "",
             isModalOpen: false,
-            modalmessage:''
         }
+        this.modalmessage = ``;
     }
-    handlechange=(evt)=>{
-        this.exchangeinfo={};
-        const {name,value}=evt.target;
-        this.exchangeinfo[name]=value;
-        evt.preventDefault();
+    // handlechange = (evt) => {   
+    //     this.exchangeinfo = {};
+    //     const { name, value } = evt.target;
+    //     this.exchangeinfo[name] = value;
+    //     evt.preventDefault();
+    // }
+    simpleexchange = async () => {
+        console.log(this.exchangeinfo);
+        let logininfo = JSON.parse(localStorage.getItem('logininfo'));
+        let email = logininfo.email_user;
+        let gamename = this.game_name;
+        let gamelist = JSON.parse(localStorage.getItem('gamelist'));
+        let game_num = gamelist.filter((game) => { return game.name_game === gamename });
+        let exchangerate = JSON.parse(localStorage.getItem('exchangerate'));
+        let ex_rate = exchangerate.filter((rate) => { return rate.idx_game === game_num[0].idx_game });
+        let characterInfo = [];
+        for (let num in this.exchangeinfo) {
+            let character = {};
+            character.character_name = this.exchangeinfo[num].char_name;
+            character.change_Gmoney = this.exchangeinfo[num].change_money;
+            character.change_Pmoney = this.exchangeinfo[num].change_platform;
+            character.ex_rate = ex_rate[0].exchange_rate;
+            character.moneydir = this.money_direction;
+            character.fee = this.exchangeinfo[num].fee;
+            if (character.moneydir === "G2P") {
+                character.change_Gmoney=-1*parseInt(character.change_Gmoney);
+            } else {
+                character.change_Pmoney=-1*parseInt(character.change_Pmoney);
+            }
+            characterInfo.push(character);
+        }
+        console.log(characterInfo);
+        await request('post','/server/exchange/saveinfo',{"userinfo":{"email":email},"characterInfo":characterInfo,});
     }
-    isOpen = (message) => {
-        console.log('눌림');
+    isOpen = (exchangeinfo, money_direction, game_name) => {
+        if (!this.state.modal) {
+            let logininfo = JSON.parse(localStorage.getItem('logininfo'));
+            let total_platform = logininfo.money_platform;
+            let platform_result = 0;
+            let platformdata = "";
+
+            let fee_data = "";
+            let fee_result = 0;
+
+            this.modalmessage = [];
+            this.modalmessage.push(<p key={3}>{game_name + " " + money_direction}</p>);
+
+            for (const i in exchangeinfo) {
+                let data = "";
+                let result = 0;
+                let fee = 0;
+                if (money_direction === "G2P") {
+                    result = parseInt(exchangeinfo[i].origin_money) - parseInt(exchangeinfo[i].change_money);
+                    data = <p key={i}>{exchangeinfo[i].char_name + " : " + exchangeinfo[i].origin_money + " - " + exchangeinfo[i].change_money + " = " + result}</p>;
+                    platform_result += parseInt(exchangeinfo[i].change_platform);
+                    platformdata += " + " + exchangeinfo[i].change_platform;
+
+                    fee = Math.round((parseInt(exchangeinfo[i].change_platform) / 20) / 10) * 10;
+                    exchangeinfo[i].fee = fee;
+                    fee_result += fee;
+                    fee_data += fee + " + ";
+                    if (parseInt(i) === exchangeinfo.length - 1) {
+                        platformdata += " - " + fee_result;
+                        platform_result -= fee_result;
+
+                    }
+                } else {
+                    result = parseInt(exchangeinfo[i].origin_money) + parseInt(exchangeinfo[i].change_money);
+                    data = <p key={i}>{exchangeinfo[i].char_name + " : " + exchangeinfo[i].origin_money + " + " + exchangeinfo[i].change_money + " = " + result}</p>;
+                    platform_result -= parseInt(exchangeinfo[i].change_platform);
+                    platformdata += " - " + exchangeinfo[i].change_platform;
+                }
+
+                this.modalmessage.push(data);
+            }
+            platform_result += total_platform;
+            fee_data = fee_data.substr(0, fee_data.length - 3);
+            this.modalmessage.push(<p key={4}>{"수수료 : " + fee_data + " = " + fee_result}</p>);
+            this.modalmessage.push(<p key={5}>{"플랫폼 머니 : " + total_platform + platformdata + " = " + platform_result}</p>);
+            this.money_direction = money_direction;
+            this.game_name = game_name;
+            this.exchangeinfo = exchangeinfo;
+        }
         this.setState(prevstate => ({
             modal: !prevstate.modal,
-            modalmessage:message,
         }));
     }
     isToggle = () => {
-            this.setState(prevstate => ({
-               isModalOpen:!prevstate.isModalOpen,
-            }));
-        }
+        this.setState(prevstate => ({
+            isModalOpen: !prevstate.isModalOpen,
+        }));
+    }
     componentDidMount = () => {
         let gamelist = JSON.parse(localStorage.getItem('gamelist'));
         let exchangerate = JSON.parse(localStorage.getItem('exchangerate'));
@@ -42,22 +117,21 @@ class Main extends Component {
 
             for (let num in gamelist) {
                 let character = characterinfo.filter((character) => {
-                    return character.idx_game ===parseInt(num)+1;
+                    return character.idx_game === parseInt(num) + 1;
                 });
                 let chan = "10P = " + exchangerate[num].exchange_rate + "0";
-                list.push(<div key={num}><Chart name={gamelist[num].name_game} number={parseInt(num)+1} characterinfo={character} change={chan} img={gamelist[num].image_path} gameintro={gamelist[num].game_intro} isOpen={this.isOpen} alt="" handlechange={this.handlechange}/></div>);
+                list.push(<div key={num}><Chart name={gamelist[num].name_game} number={parseInt(num) + 1} characterinfo={character} change={chan} img={gamelist[num].image_path} gameintro={gamelist[num].game_intro} isOpen={this.isOpen} alt="" exchange_rate={exchangerate[num].exchange_rate} /></div>);
             }
         }
         else {
             for (let num in gamelist) {
                 let chan = "10P = " + exchangerate[num].exchange_rate + "0";
-                list.push(<div key={num}><Chart name={gamelist[num].name_game} change={chan} img={gamelist[num].image_path} gameintro={gamelist[num].game_intro} isOpen={this.isOpen} alt="" isToggle={this.isToggle}/></div>);
+                list.push(<div key={num}><Chart name={gamelist[num].name_game} change={chan} img={gamelist[num].image_path} gameintro={gamelist[num].game_intro} isOpen={this.isOpen} alt="" isToggle={this.isToggle} exchange_rate={exchangerate[num].exchange_rate} /></div>);
             }
         }
         this.setState({
             gamelist: list
         });
-
     }
     render() {
         return (
@@ -68,17 +142,17 @@ class Main extends Component {
                 <div className="body">
                     {this.state.gamelist}
                 </div>
-                <Modal isOpen={this.state.modal} toggle={this.isOpen}>
-                    <ModalHeader toggle={this.isOpen}>바로 결제하시겠습니까?</ModalHeader>
+                <Modal isOpen={this.state.modal} toggle={() => { this.isOpen([], "", "") }}>
+                    <ModalHeader toggle={() => { this.isOpen([], "", "") }}>바로 결제하시겠습니까?</ModalHeader>
                     <ModalBody>
-                        {this.state.modalmessage}
-                        <Button color="success">이것만 결제할래요.</Button>
-                       <Button color="link"><Link to="/exchange">다른 게임도 볼래요.</Link></Button>
+                        {this.modalmessage}
+                        <Button color="success" onClick={this.simpleexchange}>이것만 결제할래요.</Button>
+                        <Button color="link"><Link to="/exchange" params={{ exchangeinfo: this.exchangeinfo, game_name: this.game_name, money_direction: this.money_direction }}>다른 게임도 볼래요.</Link></Button>
                     </ModalBody>
                 </Modal>
-                
+
                 {/* 헤더와 로그인폼 끝 (상욱 07.25) */}
-                <LoginModal isOpen={this.state.isModalOpen} isToggle={this.isToggle}/>
+                <LoginModal isOpen={this.state.isModalOpen} isToggle={this.isToggle} />
 
             </div>
 
